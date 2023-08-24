4166ef90251c92ec51379dabee619657105a170b
HanWol <hannahwolfson05@gmail.com>
2023-08-23 21:10:21 -0500

    adding logic and stuff from previous commits bc my laptop didn't commit stuff correctly??

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,25 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await initdb();
+		const tx = jateDb.transaction('jate', 'readwrite');
+		const store = tx.objectStore('jate');
+    const request = store.put(content);
+    const result = await request;
+    console.log('🚀 - data saved to the database', result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const openJate = await openDB('jate', 1);
+  const transaction = openJate.transaction('jate', 'readonly');
+  const storeAllJate = transaction.objectStore('jate');
+  const getAllReq = storeAllJate.getAll();
+  const final = await getAllReq;
+  
+  console.log('result.value', final);
+  return final;
+};
 
 initdb();
--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,13 +1,29 @@
 const butInstall = document.getElementById('buttonInstall');
+let deferredPrompt;
 
 // Logic for installing the PWA
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
-    
+    event.preventDefault()
+    deferredPrompt = event;
+    butInstall.style.display = "flex";
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    
+    deferredPrompt.prompt();
+    const choice = await deferredPrompt.userChoice;
+    if (choice === 'accepted'){
+        console.log("Installation accepted");
+    } else {
+        console.log("Installation rejected");
+    }
+    butInstall.style.display = "none";
+    deferredPrompt = null;
+});
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('Successfully installed!');
+});
